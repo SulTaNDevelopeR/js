fd748e1fe0ce67b0d2ff360e62e11574f4838657
SulTaNDevelopeR <93026733+SulTaNDevelopeR@users.noreply.github.com>
2021-12-26 21:11:31 +0500

    Add files via upload

---
+++ js/script.js.js
@@ -0,0 +1,21 @@
+// function showDialog(){
+//     alert("hello word");
+// }
+// // alert("hello word")
+
+document.getElementById("text").onclick =function(){
+    // alert("Muhammad Javeed Sultan");
+    document.getElementById("Result").innerHTML =" alert('Muhammad Javeed Sultan')";
+}
+document.getElementById("num").onclick =function(){
+    var x=5;
+    var y=12;
+    var sum = x+y;
+     alert(sum)
+   
+
+}
+//  document.getElementById("var-name").onclick = function(){
+
+//     document.getElementById("output").innerHTML =
+// }
